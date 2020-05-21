d1f708a780d65c4d9a5cddb63a663bea27225c96
Stauffer <Dave.Stauffer@spireenergy.com>
2020-05-21 09:01:12 -0500

    first commit

---
+++ src/test/java/com/stauffer/recipe/SpringRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stauffer.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
